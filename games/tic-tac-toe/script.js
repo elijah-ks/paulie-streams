e3e5ad7345db7e6106f532a98bd4909e2f32eae6
elijah-ks <eksabado@icloud.com>
2025-05-04 03:53:42 +0000

    Vinnie Bug Fix

--- games/tic-tac-toe/script.js
+++ games/tic-tac-toe/script.js
@@ -4,7 +4,7 @@ const status = document.getElementById('status');
 const restartBtn = document.getElementById('restart');
 
 const PAULIE_IMG = '/paulie-streams/assets/transparent.png';
-const VINNIE_IMG = '/paulie-streams/assets/vinnie.png';
+const VINNIE_IMG = '/paulie-streams/assets/Vinnie.png';
 
 
 let gameOver = false;
